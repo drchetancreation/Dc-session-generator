0070a00418498318633c4e342ba9eaef5657cff8
drchetancreation <107134390+drchetancreation@users.noreply.github.com>
2022-07-20 12:01:46 +0530

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -3,7 +3,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
 
-
 @Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not MUST_JOIN:  # Not compulsory
@@ -13,7 +12,7 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/mbbsq" + MUST_JOIN
+                link = "https://t.me/" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
