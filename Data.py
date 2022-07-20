f5debd6fcc89c830797b8ac20a0c565142c3ce1f
drchetancreation <107134390+drchetancreation@users.noreply.github.com>
2022-07-20 11:08:00 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -28,12 +28,12 @@ You can use me to generate pyrogram and telethon string session. Use the below b
     # Rest Buttons
     buttons = [
         [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")],
-        [InlineKeyboardButton("👨‍💻 Repo 👨‍💻", url="https://github.com/LEGEND-AI/STRING-GENERATE")],
+        [InlineKeyboardButton("👨‍💻 Repo 👨‍💻", url="https://github.com/drchetancreation/STRING-GENERATE")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton(" About", callback_data="about")
         ],
-        [InlineKeyboardButton("🇮🇳 Owner 🇮🇳", url="https://t.me/LegendBoy_XD")],
+        [InlineKeyboardButton("🇮🇳 Owner 🇮🇳", url="https://telegram.me/mbbsq")],
     ]
 
     # Help Message
